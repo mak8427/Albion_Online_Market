a302d96d2cace38325946e1990f0de5ff335d21a
Davide <davide.mattioli.2001@gmail.com>
2024-07-29 20:40:34 +0200

    Init

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
-a=1
-b=2
-c=3
-print(a+b+c)
+import  requests
+import polars as pl
+response = requests.get("https://europe.albion-online-data.com/api/v2/stats/prices/T4_Soul.json?qualities=1")
+#json to dict
+data = response.json()
+
+df = pl.DataFrame(data)
+df.write_csv("T4_SOUL_prices_polars.csv")
+print(df )
