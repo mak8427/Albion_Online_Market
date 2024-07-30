2f40601f6a54efc69c0a96f5bcb3a1d0842dcd60
Bashar Jaan Khan <basharjaankhan@gmail.com>
2024-07-30 12:19:33 +0000

    feat(black): fix formatter; remove flake

--- Main.py
+++ Main.py
@@ -3,17 +3,40 @@ import polars as pl
 
 
 city_dict = {}
-cities = ["Thetford", "Bridgewatch", "Martlock", "Lymhurst", "Fort Sterling", "Caerleon"]
+cities = [
+    "Thetford",
+    "Bridgewatch",
+    "Martlock",
+    "Lymhurst",
+    "Fort Sterling",
+    "Caerleon",
+]
 for city in cities:
     try:
         city_dict[city] = pl.read_csv(f"{city}.csv")
-        NOT_LOADED=False
+        NOT_LOADED = False
     except:
-        NOT_LOADED=True
-        city_dict[city] = pl.DataFrame({"item_id": [], "city": [], "quality": [], "sell_price_min": [], "sell_price_min_date": [], "sell_price_max": [], "sell_price_max_date": [], "buy_price_min": [], "buy_price_min_date": [], "buy_price_max": [], "buy_price_max_date": []})
+        NOT_LOADED = True
+        city_dict[city] = pl.DataFrame(
+            {
+                "item_id": [],
+                "city": [],
+                "quality": [],
+                "sell_price_min": [],
+                "sell_price_min_date": [],
+                "sell_price_max": [],
+                "sell_price_max_date": [],
+                "buy_price_min": [],
+                "buy_price_min_date": [],
+                "buy_price_max": [],
+                "buy_price_max_date": [],
+            }
+        )
 
-response = requests.get("https://europe.albion-online-data.com/api/v2/stats/prices/T4_Soul.json?qualities=1")
-#json to dict
+response = requests.get(
+    "https://europe.albion-online-data.com/api/v2/stats/prices/T4_Soul.json?qualities=1"
+)
+# json to dict
 data = response.json()
 df = pl.DataFrame(data)
 
@@ -21,10 +44,19 @@ df = pl.DataFrame(data)
 for city in cities:
     if NOT_LOADED:
         city_dict[city] = df.filter(pl.col("city") == city)
-    if city_dict[city]["sell_price_min_date"][-1]!= df.filter(pl.col("city")==city)["sell_price_min_date"][-1]:
+    if (
+        city_dict[city]["sell_price_min_date"][-1]
+        != df.filter(pl.col("city") == city)["sell_price_min_date"][-1]
+    ):
         # if the last timestamp is different
-        city_dict[city] = city_dict[city].merge_sorted(df.filter(pl.col("city") == city),key="item_id")
+        city_dict[city] = city_dict[city].merge_sorted(
+            df.filter(pl.col("city") == city), key="item_id"
+        )
     else:
-        print(f"{city} is up to date", city_dict[city]["sell_price_min_date"][-1], df.filter(pl.col("city")==city)["sell_price_min_date"][-1])
+        print(
+            f"{city} is up to date",
+            city_dict[city]["sell_price_min_date"][-1],
+            df.filter(pl.col("city") == city)["sell_price_min_date"][-1],
+        )
 
     city_dict[city].write_csv(f"{city}.csv")
