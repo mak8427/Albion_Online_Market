baa8cc3d39bf6d7f1a7ef883c768dd9f2a3d51b4
Davide <davide.mattioli.2001@gmail.com>
2024-07-30 11:39:10 +0200

    Function Api updated

--- Main.py
+++ Main.py
@@ -5,15 +5,28 @@ import polars as pl
 city_dict = {}
 cities = ["Thetford", "Bridgewatch", "Martlock", "Lymhurst", "Fort Sterling", "Caerleon"]
 for city in cities:
-    city_dict[city] = pl.read_csv(f"{city}.csv")
+    try:
+        city_dict[city] = pl.read_csv(f"{city}.csv")
+        NOT_LOADED=False
+    except:
+        NOT_LOADED=True
+        city_dict[city] = pl.DataFrame({"item_id": [], "city": [], "quality": [], "sell_price_min": [], "sell_price_min_date": [], "sell_price_max": [], "sell_price_max_date": [], "buy_price_min": [], "buy_price_min_date": [], "buy_price_max": [], "buy_price_max_date": []})
 
 response = requests.get("https://europe.albion-online-data.com/api/v2/stats/prices/T4_Soul.json?qualities=1")
 #json to dict
 data = response.json()
 df = pl.DataFrame(data)
+
+
 for city in cities:
-    city_dict[city] = city_dict[city].merge_sorted(df.filter(pl.col("city") == city),key="item_id")
+    if NOT_LOADED:
+        city_dict[city] = df.filter(pl.col("city") == city)
+    if city_dict[city]["sell_price_min_date"][-1]!= df.filter(pl.col("city")==city)["sell_price_min_date"][-1]:
+        city_dict[city] = city_dict[city].merge_sorted(df.filter(pl.col("city") == city),key="item_id")
+    else: print (f"{city} is up to date", city_dict[city]["sell_price_min_date"][-1], df.filter(pl.col("city")==city)["sell_price_min_date"][-1])
+
     city_dict[city].write_csv(f"{city}.csv")
+
 
 
 
